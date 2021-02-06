46a07ac1f263f2d7cf410f384ae415c85dcf15bc
dantezunino <dantealtierizunino@gmail.com>
2021-02-06 12:34:59 -0300

    Reconocimiento de click derecho en kivy

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 import kivy
+from kivy.config import Config
+Config.set('input', 'mouse', 'mouse,disable_multitouch')
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.gridlayout import GridLayout
@@ -7,6 +9,8 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.modalview import ModalView
 from kivy.uix.button import Button
 from kivy.input.providers import mouse
+from kivy.clock import Clock
+from kivy.core.window import Window
 import random
 import string
 
@@ -17,6 +21,7 @@ labelFilas = []
 labelColumnas = []
 winCondition = []
 sisi = []
+clickerio = ["left"]
 
 def setWinCondition():
     for l in lili:
@@ -65,19 +70,33 @@ def make_bitseq(s, k, lili):
     for bini in listaMasGrande:
         lili.append(bini)
 
-
 def botonio(a):
-    a.background_color=(0,0,0,1)
-    a.disabled=True
-    winCondition.remove(winCondition[0])
-    if len(winCondition) != 0:
-        pass
+    if clickerio[0]=="right" or clickerio[0]=="middle":
+        a.background_normal=''
+        a.background_disabled_down=''
+        a.background_color=(0.6,0.4,0.4,0.7)
     else:
-        MyLayout.winScreen(MyLayout)
-        winCondition.append("1")
+        a.background_normal=''
+        a.background_disabled_down=''
+        a.background_color=(0.25,0.25,0.25,1)
+        a.disabled=True
+        winCondition.remove(winCondition[0])
+        if len(winCondition) != 0:
+            pass
+        else:
+            MyLayout.winScreen(MyLayout)
+            winCondition.append("1")
 
 def loser(b):
-    MyLayout.loseScreen(MyLayout)
+    print(clickerio[0])
+    if clickerio[0]=="right" or clickerio[0]=="middle":
+        b.background_disabled_normal=''
+        b.background_disabled_down=''
+        b.background_color=(0.6,0.4,0.4,0.7)
+        b.disabled=True
+    if clickerio[0]=="left":
+        pass
+        #MyLayout.loseScreen(MyLayout)
 
 def comprension(kk):
     tamanuski = int(sisi[0])
@@ -122,6 +141,18 @@ def comprension(kk):
         h += 1
 
 class MyLayout(Widget):
+        
+    def on_touch_down(self, touch, after=False):
+        if after:
+            clickerio[0] = touch.button
+            #mm = touch.pos
+            #for widget in self.walk():
+            #    print(widget)
+            return
+        else:
+            Clock.schedule_once(lambda dt: self.on_touch_down(touch, True))
+            return super(MyLayout, self).on_touch_down(touch)
+
     def chhh(self):
         limpieza()
         choro = self.ids.pcho.text
@@ -150,48 +181,55 @@ class MyLayout(Widget):
         loseLabel = Label(text="Mal ahí. Perdiste. Sos un gil")
         view.add_widget(loseLabel)
         view.open()
+    
+    def something(self):
+        self.ids.caroso.load_previous()
+        self.ids.nonogram.clear_widgets()
 
     def jueguito(self):
+        
         self.chhh()
-        view = ModalView(size_hint=(None, None), size=(600, 600))
+        #self.view = ModalView(size_hint=(None, None), size=(600, 600))
         totoro = int(sisi[0])
-        tablita = GridLayout(cols=totoro)
-        superBoxy = BoxLayout(orientation='vertical')
-        boxy = BoxLayout(orientation='horizontal')
-        boxyLabel = BoxLayout(size_hint=(0.2,1), orientation='vertical')
-        boxyTop = BoxLayout(size_hint=(1, 0.3), orientation='horizontal')
-        blackspace = BoxLayout(size_hint=(0.2, 1), orientation='horizontal')
-        boxyColu = BoxLayout(size_hint=(1, 1), orientation='horizontal')
+        self.tablita = GridLayout(cols=totoro)
+        self.superBoxy = BoxLayout(orientation='vertical')
+        self.boxy = BoxLayout(orientation='horizontal')
+        self.boxyLabel = BoxLayout(size_hint=(0.2,1), orientation='vertical')
+        self.boxyTop = BoxLayout(size_hint=(1, 0.3), orientation='horizontal')
+        self.blackspace = BoxLayout(size_hint=(0.2, 1), orientation='horizontal')
+        self.boxyColu = BoxLayout(size_hint=(1, 1), orientation='horizontal')
         for bini in lili:
             if bini == "0":
-                sat = Button(background_color=(1,1,1,1), on_press=loser)
-                sat.bind(on_release=view.dismiss)
-                tablita.add_widget(sat)
+                self.sat = Button(background_down="",background_color=(1,1,1,1), on_release=loser)
+                #self.sat.bind(on_press=(trigger_action(duration=0.1))
+                self.tablita.add_widget(self.sat)
             if bini == "1":
-                bat = Button(background_color=(1,1,1,1), on_press=botonio)
-                tablita.add_widget(bat)
+                self.bat = Button(background_down="", background_color=(1,1,1,1), on_press=botonio)
+                self.tablita.add_widget(self.bat)
         comprension(self)
-        blackspace.add_widget(Label(text=" "))
-        blackspace.add_widget(Label(text=" "))
+        self.blackspace.add_widget(Button(text="BACK", background_color=(0,0,0.5,0.5), on_press=lambda a:self.something()))
         for elemento in labelFilas:
-            tic = Label(text=elemento)
-            boxyLabel.add_widget(tic)
+            self.tic = Label(text=elemento)
+            self.boxyLabel.add_widget(self.tic)
         for hache in labelColumnas:
             pasadena = hache.split(" ")
             ars = ""
             for pas in pasadena:
                 ars += pas + "\n" + " " + "\n"
-            mic = Label(text=ars, font_size=12)
-            boxyColu.add_widget(mic)
-        boxy.add_widget(boxyLabel)
-        boxy.add_widget(tablita)
-        boxyTop.add_widget(blackspace)
-        boxyTop.add_widget(boxyColu)
-        superBoxy.add_widget(boxyTop)
-        superBoxy.add_widget(boxy)
-        view.add_widget(superBoxy)
-        view.open()
-
+            self.mic = Label(text=ars, font_size=12)
+            self.boxyColu.add_widget(self.mic)
+        self.boxy.add_widget(self.boxyLabel)
+        self.boxy.add_widget(self.tablita)
+        self.boxyTop.add_widget(self.blackspace)
+        self.boxyTop.add_widget(self.boxyColu)
+        self.superBoxy.add_widget(self.boxyTop)
+        self.superBoxy.add_widget(self.boxy)
+        #self.view.add_widget(self.superBoxy)
+        #self.view.open()
+        
+        #self.ids.bigLayout.clear_widgets()
+        self.ids.nonogram.add_widget(self.superBoxy)
+        self.ids.caroso.load_next()
 
 class MyApp(App):
     def build(self):
